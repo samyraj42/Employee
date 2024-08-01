5ca5c3619fd598fb1861fa8ea3c03190b069cc3e
samyraj42 <45854906+samyraj42@users.noreply.github.com>
2024-08-01 11:52:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace StackArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            StackArray stack = new StackArray();
+            int[] nums = new int[] { 1, 2 ,1 ,3, 2 ,1, 4 ,2 };
+            var arrs = stack.PrintPop(nums);
+            StringBuilder sb = new StringBuilder();
+            foreach(int n in arrs)
+            {
+                sb.Append(n + ", ");
+            }
+            Console.Write(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
